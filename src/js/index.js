9b4c1e5c1460d9972703feea6cfdc70e06e79f2d
oiejaum032 <138623357+oiejaum032@users.noreply.github.com>
2023-07-09 16:37:44 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
 /* 
-  O que precisamos fazer? - quando passar o mouse em cima do personagem na lista temos que adicionar a borda azul de seleção na imagem pequena do personagem e mostrar a imagem, o nome e o texto grande do personagem que está selecionado
+  O que precisamos fazer? - quando passar o mouse em cima do personagem na lista temos que adicionar a borda azul de seleção na imagem pequena do personagem e mostrar a imagem, o nome e o texto grande do personagem que está selecionado
 
     OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
         passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
-
+        
         passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
         
         passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele 
@@ -15,12 +15,17 @@
         passo 4 - alterar a descrição do personagem grande
 */
 
-//OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
-//passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
+// OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
+// passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
+
+window.addEventListener('load', function() {
+    alert('Bem vindo ao meu primeiro site , aqui demonstro um pouco do conhecimento que estou adquirindo estudando HTML,CSS e JS');
+  });
 
 const personagens = document.querySelectorAll('.personagem');
 
-//passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
+
+// passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
 
@@ -28,30 +33,40 @@ personagens.forEach((personagem) => {
             window.scrollTo({top: 0, behavior: 'smooth'});
         }
 
-            //passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele
-            removerSelecaoDoPersonagem();
+        // passo 3 - verificar se já existe um personagem selecionado, se sim, devemos remover a seleção dele
+        removerSelecaoDoPersonagem();
 
         personagem.classList.add('selecionado');
 
-
-        //OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
+        // OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
+        
         //passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        //passo 2 - alterar a imagem do personagem grande
-        const idPersonagem = personagem.attributes.id.value;
+        alterarImagemPersonagemSelecionado(personagem);
 
-        imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+        // passo 3 - alterar o nome do personagem grande
+        alterarNomePersonagemSelecionado(personagem);
 
-        //passo 3 - alterar o nome do personagem grande
-        const nomePersonagem = document.getElementById('nome-personagem');
-
-        nomePersonagem.innerText = personagem.getAttribute('data-main');
-
-    //passo 4 - alterar a descrição do personagem grande
-    const descricaoPersonagem = document.getElementById('descricao-personagem');
-    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+        // passo 4 - alterar a descrição do personagem grande
+        alterarDescricaoPersonagem(personagem);
     })
 })
+
+function alterarDescricaoPersonagem(personagem) {
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
+    descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+}
+
+function alterarNomePersonagemSelecionado(personagem) {
+    const nomePersonagem = document.getElementById('nome-personagem');
+    nomePersonagem.innerText = personagem.getAttribute('data-name');
+}
+
+function alterarImagemPersonagemSelecionado(personagem) {
+    const imagemPersonagemGrande = document.querySelector('.personagem-grande');
+    // passo 2 - alterar a imagem do personagem grande
+    const idPersonagem = personagem.attributes.id.value;
+    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+}
 
 function removerSelecaoDoPersonagem() {
     const personagemSelecionado = document.querySelector('.selecionado');
